298baba626580ca319fe6fb42f4204424ce40c89
Ahmed Ossama <ahmedossama640@gmail.com>
2024-12-29 14:19:22 +0200

    Create DTOs

---
+++ app/Core/Application/DTOs/OrderDTO.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Core\Application\DTOs;
+
+use App\Core\Domain\Entities\Discount;
+
+class OrderDTO {
+    public $customerId;
+    public $products;
+    public $discount;
+
+    public function __construct($customerId, $products, Discount $discount) {
+        $this->customerId = $customerId;
+        $this->products = $products;
+        $this->discount = $discount;
+    }
+
+    // sanitization may be added here
+}
+    
